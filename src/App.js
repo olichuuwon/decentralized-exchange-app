c597776aeb40e67c96cb27d3824b35576aeda7c1
jeslyn lam <jeslynlxy@hotmail.com>
2022-11-06 19:38:17 +0800

    real final updates

--- src/App.js
+++ src/App.js
@@ -20,9 +20,9 @@ import styled from 'styled-components';
 
 // localhost test net
 
-const tokenContractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+const tokenContractAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
 const wethContractAddress = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9";
-const dexContractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const dexContractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
 export const ButtonWrapper = styled.div`
   display: flex;
